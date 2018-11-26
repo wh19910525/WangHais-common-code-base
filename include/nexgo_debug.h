8e90425035698238d406aec10b6fc6a6ad33f081
qinchengqiang <qinchengqiang@xgd.com>
2018-11-26 16:09:26 +0800

    add nexgo_debug.h

---
+++ include/nexgo_debug.h
@@ -0,0 +1,85 @@
+#ifndef __NEXGO_DEBUG__
+#define __NEXGO_DEBUG__
+
+///////////////////////////////////////////////
+/*
+ * 控制 debug 开关;
+ */
+#define DEBUG_FLAG
+//#undef  DEBUG_FLAG
+
+/*
+ * 如果 使用 Android libs, 添加以下 这个flag;
+ *
+ * LOCAL_CFLAGS += -DBUILD_ANDROID
+ *
+ */
+
+/*
+ * 如果 使用NDK编译, 就要使用 NDk里 提供的 libs, 需要 添加以下 这个flag;
+ *
+ * LOCAL_CFLAGS += -DUSE_NDK_BUILD
+ *
+ */
+
+/*
+ * 如果没有 定义 Android LOG_TAG, 就使用 这个默认的TAG;
+ */
+#ifndef LOG_TAG
+#define LOG_TAG "wanghai_debug"
+#endif
+
+/*
+ * 如果没有 定义 MY_TAG, 就使用 这个默认的TAG;
+ */
+#ifndef MY_TAG
+#define MY_TAG "<<-- wanghai-debug -->>"
+#endif
+
+///////////////////////////////////////////////
+#ifdef BUILD_ANDROID
+
+#ifdef USE_NDK_BUILD
+
+#include <sys/system_properties.h>
+
+#include <android/log.h>
+
+#define ALOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+#define ALOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+
+#else //USE_NDK_BUILD
+
+#include <utils/Log.h>
+
+#include <cutils/properties.h>
+
+#endif //USE_NDK_BUILD
+
+#ifdef DEBUG_FLAG
+#define NEXGO_DEBUG(fmt, ...) \
+    ALOGE(MY_TAG " @%s, line=[%d], " fmt, __FUNCTION__, __LINE__, ##__VA_ARGS__);
+#else
+    #define NEXGO_DEBUG(fmt, ...) //
+#endif
+
+#define NEXGO_INFO(fmt, ...) \
+    ALOGE(MY_TAG " @%s, line=[%d], " fmt, __FUNCTION__, __LINE__, ##__VA_ARGS__);
+
+#else //BUILD_ANDROID
+
+#ifdef DEBUG_FLAG
+#define NEXGO_DEBUG(fmt, ...) \
+    printf(MY_TAG " @%s, line=[%d], " fmt, __FUNCTION__, __LINE__, ##__VA_ARGS__);
+#else
+    #define NEXGO_DEBUG(fmt, ...)
+#endif
+
+#define NEXGO_INFO(fmt, ...) \
+    printf(MY_TAG " @%s, line=[%d], " fmt, __FUNCTION__, __LINE__, ##__VA_ARGS__);
+
+#endif //BUILD_ANDROID
+
+
+#endif //__NEXGO_DEBUG__
+
