a61946062d553065e28df883f5ddc63a4b5c8f8e
qinchengqiang <qinchengqiang@xgd.com>
2018-11-30 16:19:29 +0800

    add byteArrayToHexString, intArrayToString, floatArrayToString

--- java/wanghai/util/Utils.java
+++ java/wanghai/util/Utils.java
@@ -1,3 +1,13 @@
+/*******************************************************
+ *******************************************************
+ **
+ ** Created By           : wanghai(frank)
+ ** Created Date         : 2018-11-17
+ ** Description          : java 通用工具类;
+ **
+ *******************************************************
+ *******************************************************/
+
 package wanghai.util;
 
 import java.io.ByteArrayOutputStream;
@@ -8,10 +18,6 @@ import java.io.OutputStream;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
-
-/**
- * Created by wanghai on 2018/7/19.
- */
 
 public class Utils {
 
@@ -37,7 +43,7 @@ public class Utils {
     }
 
     /*
-     * Byte数组 转换为 字符串;
+     * 把 byte数组 转换为 字符串;
      *
      *      如果 数据里有'\0'就停止;
      */
@@ -50,6 +56,85 @@ public class Utils {
                 break;
             }
             strbuf.append((char) b[i]);
+        }
+
+        return strbuf.toString();
+    }
+
+
+    /**
+     * 把 byte数组 转换为 16进制 字符串
+     *
+     * @param byteArr 数组;
+     *
+     * @return 转换后的 16进制 字符串;
+     *
+     */
+    public static String byteArrayToHexString(byte[] byteArr) {
+        return byteArrayToHexString(byteArr, false);
+    }
+
+    /**
+     * 把 byte数组 转换为 16进制 字符串
+     *
+     * @param byteArr 数组;
+     * @param useSpaceToSeparate flag: 是否使用 空格作为分隔符;
+     *
+     * @return 转换后的 16进制 字符串;
+     *
+     */
+    public static String byteArrayToHexString(byte[] byteArr, boolean useSpaceToSeparate) {
+        String stmp = "";
+        StringBuilder sb = new StringBuilder("");
+        for (int n = 0; n < byteArr.length; n++) {
+            stmp = Integer.toHexString(byteArr[n] & 0xFF);
+            sb.append((stmp.length() == 1) ? "0" + stmp : stmp);
+            if (useSpaceToSeparate) {
+                sb.append(" ");//
+            }
+        }
+        return sb.toString().toUpperCase().trim();
+    }
+
+    /**
+     * 把 int数组 转换为 字符串
+     *
+     * @param intArr 数组;
+     * @param useSpaceToSeparate flag: 是否使用 空格作为分隔符;
+     *
+     * @return 转换后的 字符串;
+     *
+     */
+    public static String intArrayToString(int[] intArr, boolean useSpaceToSeparate){ 
+        StringBuffer strbuf = new StringBuffer();
+        for (int i = 0; i < intArr.length; i++)
+        {
+            strbuf.append(""+intArr[i]);
+            if (useSpaceToSeparate) {
+                strbuf.append(" ");//
+            }
+        }
+
+        return strbuf.toString();
+    }
+
+    /**
+     * 把 float数组 转换为 字符串
+     *
+     * @param floatArr 数组;
+     * @param useSpaceToSeparate flag: 是否使用 空格作为分隔符;
+     *
+     * @return 转换后的 字符串;
+     *
+     */
+    public static String floatArrayToString(float[] floatArr, boolean useSpaceToSeparate){ 
+        StringBuffer strbuf = new StringBuffer();
+        for (int i = 0; i < floatArr.length; i++)
+        {
+            strbuf.append(""+floatArr[i]);
+            if (useSpaceToSeparate) {
+                strbuf.append(" ");//
+            }
         }
 
         return strbuf.toString();
