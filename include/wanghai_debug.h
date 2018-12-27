38ab61859d948082093534c15258553b684f6037
qinchengqiang <qinchengqiang@xgd.com>
2018-12-27 15:33:28 +0800

    1. 添加了 使用 Android native log 的描述 2. 添加了 多个调试接口;

--- include/wanghai_debug.h
+++ include/wanghai_debug.h
@@ -11,7 +11,7 @@
 //#undef  DEBUG_FLAG
 
 /*
- * 如果 使用 Android libs, 添加以下 这个flag;
+ * 如果 使用 Aosp里的 libs, 添加以下 这个flag;
  *
  * LOCAL_CFLAGS += -DBUILD_ANDROID
  *
@@ -25,24 +25,30 @@
  */
 
 /*
- * 如果没有 定义 Android LOG_TAG, 就使用 这个默认的TAG;
+ * 如果要 重新 定义 Android LOG_TAG, 就 把 以下的代码, 放到 include <wanghai_debug.h> 的 下面;
+ *
+ *   #ifndef LOG_TAG
+ *   #define LOG_TAG "new_tag"
+ *   #endif
  */
-#ifndef LOG_TAG
+#undef LOG_TAG
 #define LOG_TAG "wanghai_debug"
-#endif
-
 /*
- * 如果没有 定义 MY_TAG, 就使用 这个默认的TAG;
+ * 如果要 重新 定义 MY_TAG, 就 把 以下的代码, 放到 include <wanghai_debug.h> 的 下面;
+ *
+ *   #ifndef MY_TAG
+ *   #define MY_TAG "<<-- wanghai-debug -->>"
+ *   #endif
  */
-#ifndef MY_TAG
+#undef MY_TAG
 #define MY_TAG "<<-- wanghai-debug -->>"
-#endif
 
 ///////////////////////////////////////////////
 #ifdef BUILD_ANDROID
 
 #ifdef USE_NDK_BUILD
 
+//使用 NDK编译:
 #include <sys/system_properties.h>
 
 #include <android/log.h>
@@ -52,6 +58,7 @@
 
 #else //USE_NDK_BUILD
 
+//使用 AOSP编译:
 #include <utils/Log.h>
 
 #include <cutils/properties.h>
@@ -70,18 +77,34 @@
 
 #else //BUILD_ANDROID
 
+//使用 printf 打印:
 #ifdef DEBUG_FLAG
 #define MY_DEBUG(fmt, ...) \
-    printf(MY_TAG " @%s, line=[%d], " fmt, __FUNCTION__, __LINE__, ##__VA_ARGS__);
+    printf(MY_TAG " @%s, line=[%d], " fmt "\n", __FUNCTION__, __LINE__, ##__VA_ARGS__);
 #else
     #define MY_DEBUG(fmt, ...)
 #endif
 
 #define MY_INFO(fmt, ...) \
-    printf(MY_TAG " @%s, line=[%d], " fmt, __FUNCTION__, __LINE__, ##__VA_ARGS__);
+    printf(MY_TAG " @%s, line=[%d], " fmt "\n", __FUNCTION__, __LINE__, ##__VA_ARGS__);
 
 #endif //BUILD_ANDROID
 
+/*
+ * 常用调试接口:
+ */
+
+/* 带开关的 调试接口 */
+#define DBG         MY_DEBUG
+#define DEBUG       MY_DEBUG
+#define D           MY_DEBUG
+#define dbg         MY_DEBUG
+#define my_dbg      MY_DEBUG
+
+/* 调试接口 */
+#define INFO        MY_INFO
+#define I           MY_INFO
+#define info        MY_INFO
+#define my_info     MY_INFO
 
 #endif //__MY_DEBUG__
-
