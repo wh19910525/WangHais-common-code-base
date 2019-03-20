80104ad26f526e1de54f75a5cc49e12921db997e
frank <wanghai@xinguodu.com>
2019-03-20 10:36:08 +0800

    add if debug for c

--- include/wanghai_debug.h
+++ include/wanghai_debug.h
@@ -66,46 +66,75 @@
 
 #endif //USE_NDK_BUILD
 
-#ifdef ENABLE_DEBUG
-#define MY_DEBUG(fmt, ...) \
-    ALOGE(MY_TAG " @%s, line=[%d], " fmt, __FUNCTION__, __LINE__, ##__VA_ARGS__);
-#else
-    #define MY_DEBUG(fmt, ...) //
-#endif
-
-#define MY_INFO(fmt, ...) \
-    ALOGE(MY_TAG " @%s, line=[%d], " fmt, __FUNCTION__, __LINE__, ##__VA_ARGS__);
-
-#else //BUILD_ANDROID
-
-//使用 printf 打印:
-#ifdef ENABLE_DEBUG
-#define MY_DEBUG(fmt, ...) \
-    printf(MY_TAG " @%s, line=[%d], " fmt "\n", __FUNCTION__, __LINE__, ##__VA_ARGS__);
-#else
-    #define MY_DEBUG(fmt, ...)
-#endif
-
-#define MY_INFO(fmt, ...) \
-    printf(MY_TAG " @%s, line=[%d], " fmt "\n", __FUNCTION__, __LINE__, ##__VA_ARGS__);
+/*
+ * 在 Android 里, 使用 ALOGE;
+ */
+#define print_interface ALOGE
 
 #endif //BUILD_ANDROID
 
 /*
- * 常用调试接口:
+ * 缺省 使用 printf 打印
+ */
+#ifndef print_interface
+#define print_interface printf
+#endif
+
+/*
+ * 开关控制 打印接口;
+ */
+#ifdef ENABLE_DEBUG
+#define MY_DEBUG(fmt, ...) \
+    ((void)print_interface(MY_TAG " @%s, line:[%d], " fmt, __func__, __LINE__, ##__VA_ARGS__))
+#else
+#define MY_DEBUG(fmt, ...) //
+#endif
+
+/*
+ * 强制 打印接口;
+ */
+#define MY_INFO(fmt, ...) \
+    ((void)print_interface(MY_TAG " @%s, line:[%d], " fmt, __func__, __LINE__, ##__VA_ARGS__))
+
+/*
+ * 条件控制 打印接口;
+ */
+#define MY_INFO_IF(cond, fmt, ...) \
+    ( cond ? ((void)print_interface(MY_TAG " @%s, line:[%d], " fmt, __func__, __LINE__, ##__VA_ARGS__)) : (void)0 )
+
+/*
+ * 定义 常用调试接口 别名
  */
 
-/* 带开关的 调试接口 */
+/* 开关控制 打印接口 */
 #define DBG         MY_DEBUG
 #define DEBUG       MY_DEBUG
 #define D           MY_DEBUG
 #define dbg         MY_DEBUG
 #define my_dbg      MY_DEBUG
+#define oem_dbg     MY_DEBUG
+#define OEM_DBG     MY_DEBUG
+#define OEM_DEBUG   MY_DEBUG
+#define trance      MY_DEBUG
+#define TRANCE      MY_DEBUG
 
-/* 调试接口 */
+
+/* 强制 打印接口 */
 #define INFO        MY_INFO
 #define I           MY_INFO
 #define info        MY_INFO
 #define my_info     MY_INFO
+#define oem_info    MY_INFO
+#define OEM_INFO    MY_INFO
+#define PRINT       MY_INFO
+#define print       MY_INFO
+
+/* 条件控制 打印接口 */
+#define info_if     MY_INFO_IF
+#define INFO_IF     MY_INFO_IF
+#define print_if    MY_INFO_IF
+#define PRINT_IF    MY_INFO_IF
+#define log_if      MY_INFO_IF
+#define LOG_IF      MY_INFO_IF
 
 #endif //__MY_DEBUG__
